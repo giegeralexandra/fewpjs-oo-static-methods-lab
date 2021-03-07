6e84b0dc4aabafb36c94d925de1ed15d9d438ebc
giegeralexandra <giegeralexandra@gmail.com>
2021-03-07 14:26:46 -0500

    commit tree ab1e589636bea353e29db18164ef5400c35db1d9

--- index.js
+++ index.js
@@ -1,3 +1,20 @@
 class Formatter {
-  //add static methods here
+  static capitalize(string){
+    return string[0].toUpperCase() + string.slice(1)
+  }
+
+  static sanitize(string){
+    return string.replace(/[^A-Za-z0-9-' ]/g, '')
+  }
+
+  static titleize(string){
+    let exceptions = ['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from'];
+    let array = Formatter.capitalize(string).split(" ");
+    for (let i=0; i < array.length; i++){
+      if (!exceptions.includes(array[i])){
+        array[i] = this.capitalize(array[i])
+      }
+    }
+    return array.join(" ")
+  }
 }
